1b872973c1eb1e133a487e644793a0aedc15b235
rfgalleani <rfgalleani>
2020-07-09 15:58:36 +0100

    added default database connection

--- crm/settings.py
+++ crm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qm2-l^0cw^x#qehfpnh4ok6+vzsw4mp#yu0)jicv)8*vr2&$)y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['crm-dio.herokuapp.com']
+ALLOWED_HOSTS = ['crm-dio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -80,15 +80,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
